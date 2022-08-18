570a029c96a3e00fe192a85daebd96ff24d6c2a8
Owais Raza <owais.leghari@hotmail.com>
2022-08-19 00:13:22 +0500

    Added first two lessons

---
+++ Project 1/main.js
@@ -0,0 +1,26 @@
+//scene
+const scene = new THREE.Scene()
+//red cube
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color: 'red'})
+const mesh = new THREE.Mesh(geometry,material)
+scene.add(mesh)
+//sizes
+const sizes = {
+    width:800,
+    height:600
+}
+//camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+camera.position.x = 1
+camera.position.y = 1
+scene.add(camera)
+//renderer
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGLRenderer({
+    canvas
+})
+// renderer.setSize(sizes.height,sizes.width)
+
+renderer.render(scene,camera)
